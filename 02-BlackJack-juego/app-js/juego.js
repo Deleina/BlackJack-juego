ff86cbc4529ecdae1d7443bcf87b4ba394099b7c
Deleina llamocca <marta.deleina@gmail.com>
2022-06-21 23:21:41 -0500

    HTML y Estilos para las cartas, se creo con ayuda del framework Bootstrap

---
+++ 02-BlackJack-juego/app-js/juego.js
@@ -0,0 +1,19 @@
+/**
+ * 2C = two of clubs
+ * 2D = two of Diamons
+ * 2H = two of Hearts
+ * 2S = two of Spades
+ */
+
+//
+let deck = [];
+
+const crearDeck = () => {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+
+}
+
+crearDeck();
