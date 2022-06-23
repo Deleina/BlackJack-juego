aa4863efe168c922a777741eaf79534d30edf798
Deleina llamocca <marta.deleina@gmail.com>
2022-06-22 23:54:27 -0500

    creando funcion shuffle  que sirve para barajear las cartas, creando otra funcion que retorna tipos, especiales en un solo array llamado deck

--- 02-BlackJack-juego/app-js/juego.js
+++ 02-BlackJack-juego/app-js/juego.js
@@ -7,13 +7,37 @@
 
 //
 let deck = [];
+const tipos =['C','D','H','S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+
+function shuffle(arrayShuffle) {
+    for (let i = arrayShuffle.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = arrayShuffle[i];
+        arrayShuffle[i] = arrayShuffle[j];
+        arrayShuffle[j] = temp;
+    }
+    return arrayShuffle;
+}
+
 
 const crearDeck = () => {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
-
+    for (let i = 2; i <= 10; i++) {
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }    
+    } 
+    
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp+ tipo)
+        }
+    }    
+    console.log(deck);
+    deck=shuffle(deck)
+    console.log(deck);
+    return deck;
 }
 
 crearDeck();
